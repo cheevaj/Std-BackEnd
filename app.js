d22cdd62654be58a568866e980c293a57bfb1003
Chi Vang <Chi@tplus.la>
2024-05-03 15:57:09 +0700

    STD-5/3

---
+++ app.js
@@ -0,0 +1,11 @@
+// var first_name = "Chi";
+// var last_name = "vang";
+// console.log('Name:',first_name +" "+ last_name);
+
+let className = "API Developer by Node.js";
+console.log('Name:',className)
+
+const ip = 110;
+console.log('IP:',ip);
+ip = 112
+console.log('IP:',ip);
