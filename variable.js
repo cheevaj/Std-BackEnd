8ce9acca21bd81993c4f882a0b405326eb461fc5
Chi Vang <Chi@tplus.la>
2024-05-06 15:49:09 +0700

    chi-5/6-2

--- variable.js
+++ variable.js
@@ -8,3 +8,20 @@ console.log('pi:',pi);
 let first_name = 'Chi'
 let last_name = 'Vang'
 console.log('Name:',first_name + ' ' +last_name );
+
+let isReading = true;
+let isSleeping = false;
+
+console.log('R:',isReading,'S:',isSleeping)
+
+let value = null;
+console.log('Null:',value)
+
+let sym1 = Symbol('fool')
+console.log('sym1:',sym1)
+
+let myCar = new Object();
+myCar.make = 'Ford';
+myCar.model = 'Mustang';
+console.log('My Car:',myCar)
+console.log('My Car model:',myCar.model)
